45ee92ab8f92041809ad8e9cf079672cdbb13765
kenny oh <kennyoh517@gmail.com>
2020-07-10 08:20:40 -0400

    add env variables

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from datetime import datetime
 
+import os
 import praw
+import psycopg2
 
-reddit = praw.Reddit(client_id="wz4HVmg9UsLO-Q",
-                     client_secret="kAw1q2FhpRwkZCpFRgKFnW2h3Fg",
+reddit = praw.Reddit(client_id=os.getenv("REDDIT_CLIENT_ID"),
+                     client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
                      user_agent="ds-final-project")
+
+conn = psycopg2.connect(host=os.getenv('DB_HOST'),
+                        port=os.getenv('DB_PORT'),
+                        user=os.getenv('DB_USER'),
+                        password=os.getenv('DB_PASSWORD'),
+                        database=os.getenv('DB_DATABASE'))
+
+
 
 def cronjob():
     """
     Main cron job.
     The main cronjob to be run continuously.
     """
-    print(reddit.subreddit('kanye').active_user_count)
+    k = reddit.subreddit('kanye')
+
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO subreddits(display_name, subscribers,  active_user_count, timestamp) VALUES(%s, %s, %s, %s)",
+        (k.display_name, k.subscribers, k.active_user_count, datetime.now()))
+    conn.commit()
+    cursor.close()
+
     print("Tick! The time is: %s" % datetime.now())
